b1445c6bfcd56986cdc880f3f561e2014732c3a5
Brandon Chan <brandonhofaichan@gmail.com>
2019-09-10 13:14:39 -0500

    completed Week 1 challenge

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,37 +6,65 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            var resultSubtract = minuend - subtrahend;
+            return resultSubtract;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var resultAdd = number1 + number2;
+            return resultAdd;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else if (number1 == number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var resultMultiply = factor1 * factor2;
+            return resultMultiply;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }          
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
